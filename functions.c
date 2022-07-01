b3ec095f1bb79c6045be9f5c294a442246106fcd
vereskova <irauzun2004@gmail.com>
2022-07-01 19:59:26 +0300

    написала работающую функцию

---
+++ functions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int function(int k, int i, int N, double p) {
+    int j = 0;
+    for (k = 1; k <= N; k++)
+        for (i = 1; i < pow(k, 1.0 / 3); i++)
+        {
+            j = k - i * i * i;
+            p = pow(j, 1.0 / 3);
+
+            if (fabs(p - (int)p) < 1e-5)
+            {
+                printf("%li = %li^3 + %.0lf^3 \n", k, i, p);
+
+                break;
+            }
+        }
+
+}
